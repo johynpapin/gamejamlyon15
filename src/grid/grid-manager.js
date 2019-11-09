f28e64f53cddc299c82f16319adb991bc8cf2e2b
Nelh <nelh@localhost.localdomain>
2019-11-09 22:47:49 +0100

    maybe

--- src/grid/grid-manager.js
+++ src/grid/grid-manager.js
@@ -69,7 +69,7 @@ export default class GridManager {
       stack.push(currentCell)
       currentCell = this.grid.cells[currentCell.tile.targetX][currentCell.tile.targetY]
     }
-    if (this.grid.isFullUtensil(currentCell.tile.targetX, currentCell.tile.targetY) || !(currentCell.tile instanceof MovingTile)) {
+    if (!(currentCell.tile instanceof MovingTile) || this.grid.isFullUtensil(currentCell.tile.targetX, currentCell.tile.targetY)) {
       while (stack.length > 0) {
         currentCell = stack.pop()
         this.grid.cells[currentCell.ingredient.x][currentCell.ingredient.y].ingredient.hasMoved = true
