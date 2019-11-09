798b5f8cced381ba80b43b3ef0bddaf6efe390d3
Johyn Papin <johynpapin@protonmail.com>
2019-11-09 12:43:06 +0100

    a big black screen

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ PIXI.utils.sayHello(type)
 
 const app = new PIXI.Application()
 
+// Make the game fill the window
+app.renderer.view.style.position = 'absolute'
+app.renderer.view.style.display = 'block'
+app.renderer.autoResize = true
+app.renderer.resize(window.innerWidth, window.innerHeight)
+
 document.body.appendChild(app.view)
-
-
