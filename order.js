ea7b454179733c116ea19be4d9f4504b60497ee4
Nelh <nelh@localhost.localdomain>
2019-11-09 15:04:06 +0100

    orders-manager

--- order.js
+++ order.js
@@ -1,4 +1,4 @@
-class Order {
+export default class Order {
   constructor (recipe, currency, timer) {
     this.dish = recipe // List of ingredients/recipes
     this.earned = currency
--- orders-manager.js
+++ orders-manager.js
@@ -1,7 +1,39 @@
-class OrdersManager {
-  constructor (tics = 0, orders) {
-    this.tics = tics
+import Order from './order'
+import Orders from './orders'
+
+export default class OrdersManager {
+  constructor (orders) {
+    this.reinit()
     this.orders = orders
     this.number = orders.ordering.length
+  }
+
+  reinit () {
+    this.tics = 0
+    this.threshold = Math.floor(Math.random() * 30) + 20
+  }
+
+  createOrder () {
+    const newOrder = new Order(null, null, null)
+    this.orders.ordering.push(newOrder)
+    this.number += 1
+  }
+
+  resolvedOrder (order) {
+    this.orders.slice(this.orders.indexOf(order), 1)
+    this.number -= 1
+  }
+
+  next () {
+    if (this.number === 0) {
+      this.createOrder()
+      this.reinit()
+    } else if (this.number < 3) {
+      if (this.tics >= this.treshold) {
+        this.createOrder()
+        this.reinit()
+      }
+    }
+    this.tics = this.tics + 1
   }
 }
