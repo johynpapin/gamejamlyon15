2a6a9b8604d1b00f0b54da649d787bb19d2b5df9
math <mathilde.grapin@gmail.com>
2019-11-09 21:29:12 +0100

    tile connector

--- src/grid/tiles/tile-connector.js
+++ src/grid/tiles/tile-connector.js
@@ -1,7 +1,12 @@
 import MovingTile from './moving-tile'
 
 class TileConnector extends MovingTile {
-  constructor() {
+  constructor (x, y, nextCell) {
+    super(x, y, nextCell)
+    this.connected = false
+  }
 
+  switchConnect () {
+    this.connect = !this.connected
   }
 }
