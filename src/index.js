ef316533d6d4134df80d4e7ddd89dd2de64a05fc
Johyn Papin <johynpapin@protonmail.com>
2019-11-09 15:35:02 +0100

    webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js'
+import './index.css'
 
 let type = 'WebGL'
 
@@ -19,19 +20,35 @@ app.renderer.resize(window.innerWidth, window.innerHeight)
 document.body.appendChild(app.view)
 
 // List of all the images
-const images = []
+const resources = [{
+  name: 'orderAngleL',
+  url: '/iimages/Order_Angle_L.png'
+}, {
+  name: 'orderAngleR',
+  url: '/iimages/Order_Angle_R.png'
+}]
+
+const loader = new PIXI.Loader()
 
 // Load the images and run the `setup` function when it's done
-PIXI.loader()
-  .add(images)
+loader
+  .add(resources)
   .load(setup)
 
 // Setup the game
-function setup () {
+function setup (loader, resources) {
+  const orderAngleL = new PIXI.Sprite(resources.orderAngleL.texture)
+
+  console.log(orderAngleL)
+
+  orderAngleL.x = app.renderer.width / 2
+  orderAngleL.y = app.renderer.height / 2
+
+  app.stage.addChild(orderAngleL)
+
   app.ticker.add(delta => gameLoop(delta))
 }
 
 // The game loop
 function gameLoop (delta) {
-
 }
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Game Jam Lyon 15'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }, {
+      test: /\.css$/,
+      exclude: /(node_modules)/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }]
+  }
+}
