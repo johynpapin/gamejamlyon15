408f5bb6b601556ac112ecdf9f66d70a07a17c44
Johyn Papin <johynpapin@protonmail.com>
2019-11-09 15:54:58 +0100

    drawing

--- src/index.js
+++ src/index.js
@@ -21,11 +21,41 @@ document.body.appendChild(app.view)
 
 // List of all the images
 const resources = [{
+  name: 'arrowDown',
+  url: '/assets/Arrow_Down.png'
+}, {
+  name: 'arrowL',
+  url: '/assets/Arrow_L.png'
+}, {
+  name: 'arrowR',
+  url: '/assets/Arrow_R.png'
+}, {
+  name: 'arrowUp',
+  url: '/assets/Arrow_Up.png'
+}, {
+  name: 'cold',
+  url: '/assets/Cold_v1.png'
+}, {
+  name: 'econome',
+  url: '/assets/Econome_v1.png'
+}, {
+  name: 'friteuse',
+  url: '/assets/Friteuse_v1.png'
+}, {
+  name: 'marmite',
+  url: '/assets/Marmite_v1.png'
+}, {
+  name: 'mixer',
+  url: '/assets/Mixer_v1.png'
+}, {
   name: 'orderAngleL',
-  url: '/iimages/Order_Angle_L.png'
+  url: '/assets/Order_Angle_L.png'
 }, {
   name: 'orderAngleR',
-  url: '/iimages/Order_Angle_R.png'
+  url: '/assets/Order_Angle_R.png'
+}, {
+  name: 'mainTileNeutral',
+  url: '/assets/Main_Tile_Neutral_v1.png'
 }]
 
 const loader = new PIXI.Loader()
