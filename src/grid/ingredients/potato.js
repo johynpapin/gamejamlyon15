aa7fd2b4e8b1463f9c4e74b394ab038cd9b61633
math <mathilde.grapin@gmail.com>
2019-11-11 14:53:05 +0100

    ii

--- src/grid/ingredients/potato.js
+++ src/grid/ingredients/potato.js
@@ -2,20 +2,15 @@ import * as PIXI from 'pixi.js'
 import Ingredient from './ingredient'
 
 export default class Potato extends Ingredient {
-  constructor (x, y, grid) {
-    super(x, y, grid)
-    this.checkSprite()
-  }
-
-  checkSprite () {
-
-  }
-
   draw (delta, container, resources, offset) {
     if (!this.sprite) {
       this.sprite = new PIXI.Sprite(resources.potato.texture)
       this.sprite.anchor.set(0.5)
       container.addChild(this.sprite)
+    } else if (this.states === ['peel', 'fry']) {
+      this.sprite = new PIXI.Sprite(resources.potatoFries.texture)
+    } else if (this.states === ['peel']) {
+
     }
 
     super.draw(container, delta)
