d042da7a98da5e493bb4baad2c5d7a15b19f22b3
math <mathilde.grapin@gmail.com>
2019-11-11 12:17:55 +0100

    console

--- src/grid/utensils/utensil.js
+++ src/grid/utensils/utensil.js
@@ -18,8 +18,6 @@ export default class Utensil {
     if (this.ticks === 0 && this.targetsAreFree()) {
       this.apply(grid)
 
-      console.log(this.createdIngredients)
-
       for (let i = 0; i < this.createdIngredients.length; i++) {
         this.targetCells[i].ingredient = this.createdIngredients[i]
         this.targetCells[i].ingredient.x = this.targetCells[i].x
