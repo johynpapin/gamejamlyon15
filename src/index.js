79a3a70ee2d051ee7387d8fd3ed7d3f8f18debf5
Johyn Papin <johynpapin@protonmail.com>
2019-11-11 12:17:03 +0100

    hey

--- src/index.js
+++ src/index.js
@@ -69,6 +69,9 @@ const resources = [{
 }, {
   name: 'trash',
   url: '/assets/texture_package/Trashbin_v1-Sheet.json'
+}, {
+  name: 'waste',
+  url: '/assets/Space_Caca.png'
 }]
 
 const loader = new PIXI.Loader()
@@ -110,10 +113,6 @@ function gameLoop (resources, delta) {
 }
 
 function scaleScene () {
-<<<<<<< HEAD
-=======
-
->>>>>>> 79fb0281de552bb17240da2bac6f545086adf3f2
   let w = window.innerWidth
   let h = window.innerWidth / ratio
 
@@ -122,10 +121,6 @@ function scaleScene () {
     h = window.innerHeight
   }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 79fb0281de552bb17240da2bac6f545086adf3f2
   mainContainer.scale.x = w / 640
   mainContainer.scale.y = h / 480
   app.renderer.resize(w, h)
